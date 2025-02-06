0c58efb57b93478a1a6798f12498477a3c239c02
andreasqueri <andrea.squeri@gmail.com>
2025-02-06 15:38:21 +0100

    Update keymap.c

--- EM56G/keymap.c
+++ EM56G/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
@@ -12,6 +13,10 @@ enum custom_keycodes {
 enum tap_dance_codes {
   DANCE_0,
 };
+
+void housekeeping_task_user(void) {
+  achordion_task();
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
@@ -131,6 +136,7 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
